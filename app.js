8cd0c620231f84f7b379228773394dfa9a9c0bb8
Chase Wille <16bitart@protonmail.com>
2021-01-16 16:26:43 -0600

    Setting up transitions, more flexbox items

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const panels = document.querySelectorAll('.panel');
+const twitchPanel = document.querySelector('#twitch');
 
 panels.forEach((panel) => {
   console.log(panel);
@@ -13,3 +14,21 @@ function removeActiveClass(){
     panel.classList.remove('active');
   });
 }
+
+
+function CreateTwitchPage(){
+let twitchWidth = 1280;
+let twitchHeight = 720;
+
+let twitchChannel = new Twitch.Embed("twitch-embed", {
+  width: twitchWidth,
+  height: twitchHeight,
+  autoplay: true,
+  channel: "avanicolexd",
+  // only needed if your site is also embedded on embed.example.com and othersite.example.com
+  parent: ["embed.example.com", "othersite.example.com"],
+});
+}
+
+
+
